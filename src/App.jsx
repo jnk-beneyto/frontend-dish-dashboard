af270c7c3f197956d35f81e872f98171230cea15
J.C. Beneyto <jnk.beneyto@gmail.com>
2021-12-06 09:42:51 +0100

    first commit

---
+++ src/App.jsx
@@ -0,0 +1,13 @@
+import React from "react";
+
+function App () {
+  return (
+    <div className="flex justify-center">
+      <h1 className="text-2xl text-blue-900">
+        React and Tailwind with Vitejs!
+      </h1>
+    </div>
+  );
+}
+
+export default App;
