741c3d8c1df3b8d8b565af2db4c7d37772be1a29
J.C. Beneyto <jnk.beneyto@gmail.com>
2021-12-18 10:49:58 +0100

    refactoring component in order to use useFetch

--- src/components/DishList.jsx
+++ src/components/DishList.jsx
@@ -1,42 +1,12 @@
 import React from 'react'
-import { useEffect } from 'react'
-import { useToasts } from 'react-toast-notifications'
 import { DishCard } from './DishCard'
-import { dishService } from '../services'
-import { useDishDispatch, useDishState } from '../context/dishContext'
+import { useFetch } from '../hooks/useFetch'
 
 export const DishList = () => {
-  const { search, fetchData, dishes, error, errorMessage, isLoading, dishesFixed } = useDishState()
-  const { setToggleFetch, setDishes, setLoading, setDishesFixed } = useDishDispatch()
-  const { addToast } = useToasts()
-
-  useEffect(() => {
-    if (fetchData) {
-      setLoading(true)
-      dishService.getAll()
-        .then(data => {
-          setLoading(false)
-          setDishes(data.message)
-          setDishesFixed(data.message)
-          setToggleFetch(false)
-        })
-        .catch(e => addToast(e.message || 'Some error happened', { appearance: 'error', autoDismiss: true }))
-    }
-  }, [fetchData])
-
-  useEffect(() => {
-    if (search !== '') {
-      const filteredDishes = dishesFixed.filter(dish => dish.name.toLowerCase().includes(search.toLowerCase()))
-      setDishes(filteredDishes)
-    } else {
-      setDishes(dishesFixed)
-    }
-  }, [search])
-
-  // const { addToast } = useToasts()
+  const { error, isLoading, dishes } = useFetch()
 
   if (error) {
-    console.log(errorMessage)
+
     return (
       <div className='bg-red-400 container mx-auto grid grid-cols-1 rounded-md'>
         <h4 className='text-center py-4 text-white uppercase'> Error found</h4>
@@ -45,7 +15,7 @@ export const DishList = () => {
   } else {
     return (
       <>
-        {isLoading && !error && <div>Loading...</div>}
+        {isLoading && <div>Loading...</div>}
         {!isLoading && !error && dishes.length ? (
           <div className='grid grid-cols-1 sm:grid-cols-2'>
             {dishes.map((dish, index) => {
